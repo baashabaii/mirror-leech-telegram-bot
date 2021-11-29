01ab50597fc33a35b01c4b127fd6b6992ec68a0a
baashabaii <86482549+baashabaii@users.noreply.github.com>
2021-11-29 14:37:15 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -122,6 +122,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
+            reply_to = download.message.reply_to_message
             msg += f"<b>▬▬▬▬▬  @MSPbots ▬▬▬▬▬\n\n𝗙𝗶𝗹𝗲𝗻𝗮𝗺𝗲 :</b><code>{download.name()}</code>"
             msg += f"\n<b>𝗦𝘁𝗮𝘁𝘂𝘀 :{download.status()}</b>"
             if download.status() not in [
