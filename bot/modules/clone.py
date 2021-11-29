f026ae19492555db2bd40ed4321cfa6f14ca2738
baashabaii <86482549+baashabaii@users.noreply.github.com>
2021-11-29 19:45:53 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -23,9 +23,9 @@ def cloneNode(update, context):
         link = ''
     gdtot_link = is_gdtot_link(link)
     if gdtot_link:
+        sendMessage('Please Wait', context.bot, update)
         try:
             link = gdtot(link)
-            msg = sendMessage(f"Connecting Please Wait")
         except DirectDownloadLinkException as e:
             return sendMessage(str(e), context.bot, update)
     if is_gdrive_link(link):
