20875067721cb5dc2f74d5d3fd1c1c0c096fe3a8
baashabaii <86482549+baashabaii@users.noreply.github.com>
2021-11-30 12:30:43 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -88,6 +88,5 @@ def cloneNode(update, context):
     else:
         sendMessage('Send Gdrive or gdtot link along with command or by replying to the link by command', context.bot, update)
 
-clone_handler = CommandHandler(BotCommands.CloneCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-clone_handler = CommandHandler(BotCommands.GdToTCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+clone_handler = CommandHandler(BotCommands.CloneCommand, BotCommands.GdToTCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(clone_handler)
